7a821284bb868e10ee08e6c231e1143c5ef4d82e
aissbilel <54020857+aissbilel@users.noreply.github.com>
2023-02-16 13:53:46 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,31 @@
-// Sélectionner tous les liens du menu de navigation
-const navLinks = document.querySelectorAll('nav a');
-
-// Ajouter un gestionnaire d'événement de clic à chaque lien
-navLinks.forEach(link => {
-  link.addEventListener('click', (event) => {
-    event.preventDefault(); // Empêcher le comportement par défaut du lien
-    const sectionId = link.getAttribute('href'); // Récupérer l'identifiant de la section liée au lien
-    const section = document.querySelector(sectionId); // Sélectionner la section correspondante
-    const sections = document.querySelectorAll('section'); // Sélectionner toutes les sections
-    sections.forEach(s => s.classList.remove('active')); // Désactiver toutes les sections
-    section.classList.add('active'); // Activer la section correspondante
-  });
-});
-
-// Ajouter un gestionnaire d'événement de défilement pour activer les sections au fur et à mesure qu'elles deviennent visibles
-window.addEventListener('scroll', () => {
-  const sections = document.querySelectorAll('section');
-  sections.forEach(section => {
-    const rect = section.getBoundingClientRect();
-    if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
-      section.classList.add('active');
+// Ajoute la classe active à l'élément de menu cliqué et supprime la classe active de tous les autres éléments
+function setActiveMenu(menu) {
+  const menuItems = document.querySelectorAll('.menu a');
+  for (let i = 0; i < menuItems.length; i++) {
+    if (menuItems[i] === menu) {
+      menuItems[i].classList.add('active');
     } else {
-      section.classList.remove('active');
+      menuItems[i].classList.remove('active');
     }
+  }
+}
+
+// Défilement fluide vers la section correspondant au menu cliqué
+function scrollToSection(event) {
+  event.preventDefault();
+  const targetId = event.currentTarget.getAttribute('href');
+  const targetSection = document.querySelector(targetId);
+  window.scroll({
+    top: targetSection.offsetTop,
+    behavior: 'smooth'
   });
-});
+}
+
+// Ajoute un gestionnaire d'événements click à chaque élément de menu
+const menuItems = document.querySelectorAll('.menu a');
+for (let i = 0; i < menuItems.length; i++) {
+  menuItems[i].addEventListener('click', function(event) {
+    setActiveMenu(this);
+    scrollToSection(event);
+  });
+}
