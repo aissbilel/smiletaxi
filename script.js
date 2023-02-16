6315166101b227ea907d0b8b5cc78fa1472cd87e
aissbilel <54020857+aissbilel@users.noreply.github.com>
2023-02-16 17:49:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,17 @@
-//Sélectionnez tous les liens de navigation avec des ancres
-$('a[href*="#"]')
-  //Retirer tout lien qui ne contient qu'un dièse (#)
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    // Empêcher le comportement de clic par défaut
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
-      location.hostname == this.hostname
-    ) {
-      // Définir la cible d'ancrage
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Vérifier si la cible d'ancrage existe
-      if (target.length) {
-        // Empêcher le comportement de clic par défaut
-        event.preventDefault();
-        // Définir la vitesse de défilement et la distance entre la cible d'ancrage et le haut de la page
-        var speed = 1000; // vitesse en millisecondes
-        var offset = target.offset().top;
-        // Animer le défilement
-        $('html, body').animate({
-          scrollTop: offset
-        }, speed);
-      }
-    }
-  });
+$(function() {
+    /**
+    * Smooth scrolling to page anchor on click
+    **/
+    $("a[href*='#']:not([href='#'])").click(function() {
+        if (
+            location.hostname == this.hostname
+            && this.pathname.replace(/^\//,"") == location.pathname.replace(/^\//,"")
+        ) {
+            var anchor = $(this.hash);
+            anchor = anchor.length ? anchor : $("[name=" + this.hash.slice(1) +"]");
+            if ( anchor.length ) {
+                $("html, body").animate( { scrollTop: anchor.offset().top }, 1500);
+            }
+        }
+    });
+});
