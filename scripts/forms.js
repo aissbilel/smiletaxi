ca7eba9d34f28aa45873cb7242439a71ee2007e2
aissbilel <54020857+aissbilel@users.noreply.github.com>
2023-02-18 01:53:15 +0100

    Update forms.js

--- scripts/forms.js
+++ scripts/forms.js
@@ -1,4 +1,13 @@
-// Resize your browser for effect
+
+const navbarToggle = document.querySelector('.navbar-toggle');
+const navbarMenu = document.querySelector('.navbar-menu');
+
+navbarToggle.addEventListener('click', () => {
+  navbarMenu.classList.toggle('active');
+});
+
+
+
 
 
 const objet = document.getElementById('objet');
