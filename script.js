458382a56b2ee46f6f2e09d9f7caec96f216f35f
Bilel Aissaoui <aiss.bilel@gmail.com>
2023-04-09 17:31:25 +0200

    site

--- script.js
+++
@@ -1,6 +0,0 @@
-const navbarToggle = document.querySelector('.navbar-toggle');
-const navbarMenu = document.querySelector('.navbar-menu');
-
-navbarToggle.addEventListener('click', () => {
-  navbarMenu.classList.toggle('active');
-});
